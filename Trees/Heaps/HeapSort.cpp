adfe568ccdd3570f9da4c4c36b3d41d924605a9c
Hoax71 <devraghav538@gmail.com>
2025-05-06 23:17:39 +0530

    continuing heap

---
+++ Trees/Heaps/HeapSort.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void heapify(vector<int> &arr, int n, int i)
+{
+    int index = i;
+    int leftIndex = 2 * i;
+    int rightIndex = 2 * i + 1;
+    int largest = index;
+
+    if (leftIndex <= n && arr[leftIndex] > arr[largest])
+        largest = leftIndex;
+
+    if (rightIndex <= n && arr[rightIndex] > arr[largest])
+        largest = rightIndex;
+
+    if (index != largest)
+    {
+        swap(arr[index], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void buildHeap(vector<int> &arr, int n)
+{
+    for (int i = n / 2; i > 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+}
+
+void heapSort(vector<int> &arr, int n)
+{
+    int index = n;
+
+    while (n != 1)
+    {
+        swap(arr[index], arr[1]);
+        index--;
+        n--;
+
+        heapify(arr, n, 1);
+    }
+}
+
+int main()
+{
+    int userLength;
+    cout << "Enter the number of elements in the array: ";
+    cin >> userLength;
+
+    vector<int> userArr(userLength + 1, -1); // Incorporating -1 at the start index
+
+    cout << "Enter the elements of the array:\n";
+    for (int i = 1; i <= userLength; i++)
+        cin >> userArr[i];
+    return 0;
+
+    buildHeap(userArr, userLength);
+
+    heapSort(userArr, userLength);
+
+    cout << "Sorted Array using Heap Sort: ";
+    for (int i = 1; i <= userLength; i++)
+        cout << userArr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
